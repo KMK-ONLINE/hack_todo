e93c21a29337572d59c2c0fd55ace2b856aac4b4
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2015-11-26 09:41:37 +0700

    move the routes to a controller add basic tests

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class TasksController extends Controller {
+
+  public function index() {
+    return view('tasks', [
+      'tasks' => Task::orderBy('created_at', 'asc')->get()
+    ]);
+  }
+
+  public function store(Request $request) {
+    $validator = Validator::make($request->all(), [
+      'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+      return redirect('/')
+        ->withInput()
+        ->withErrors($validator);
+    }
+
+    $task = new Task();
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+  }
+
+  public function destroy($id) {
+    Task::findOrFail($id)->delete();
+    return redirect('/');
+  }
+
+}

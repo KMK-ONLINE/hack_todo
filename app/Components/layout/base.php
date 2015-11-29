56cad888320127b37567001efe76ded4f75735c3
Tommy Sullivan <tommy.b.sullivan@gmail.com>
2015-11-29 14:49:20 +0700

    add laravel’s elixir for assets

--- app/Components/layout/base.php
+++ app/Components/layout/base.php
@@ -19,6 +19,8 @@ class :layout:base extends :x:element {
           <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
           <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 
+          <link href={elixir('css/app.css')} rel="stylesheet" type="text" />
+
           <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
           <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
